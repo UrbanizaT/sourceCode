481bac7affd5acda49411623b31cd3122a05a0f9
Aleksander <aprokhorenkodev@gmail.com>
2026-02-11 19:21:51 +0100

    SCREEN | style: delete un used directives

--- app/src/main/java/com/example/urbanizat/ui/screens/Register.kt
+++ app/src/main/java/com/example/urbanizat/ui/screens/Register.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
