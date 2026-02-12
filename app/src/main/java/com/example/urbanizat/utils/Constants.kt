e88b770cf418a45d478539948b32c38e7a79367a
Aleksander <aprokhorenkodev@gmail.com>
2026-02-12 20:27:40 +0100

    UTILS | feat: create a file to add constants

---
+++ app/src/main/java/com/example/urbanizat/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.urbanizat.utils
+
+import androidx.compose.ui.unit.dp
+
+
+//Styles of components
+val ROUNDED_CORNERS = 12.dp
+val SPACER_HEIGHT = 12.dp
+val SPACE_INPUTS_FORM = 8.dp
