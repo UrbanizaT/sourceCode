ff569bb20c3600f7e274e7c0a498e721d0361815
Aleksander <aprokhorenkodev@gmail.com>
2026-02-12 20:29:26 +0100

    COMPONENT | style: delete un used directives

--- app/src/main/java/com/example/urbanizat/ui/components/forms/InputCustom.kt
+++ app/src/main/java/com/example/urbanizat/ui/components/forms/InputCustom.kt
@@ -1,21 +1,14 @@
 package com.example.urbanizat.ui.components.forms
 
-import android.R
-import android.graphics.Color.alpha
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
+
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.urbanizat.ui.theme.UrbanizaTTheme
 import com.example.urbanizat.utils.ROUNDED_CORNERS
 
