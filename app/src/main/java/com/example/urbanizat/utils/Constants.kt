77e5674824437934b0f3f7e515b1539acc37c956
Aleksander <aprokhorenkodev@gmail.com>
2026-02-13 11:30:16 +0100

    CONSTANTS | feat: add new constant and delete

--- app/src/main/java/com/example/urbanizat/utils/Constants.kt
+++ app/src/main/java/com/example/urbanizat/utils/Constants.kt
@@ -1,9 +1,11 @@
 package com.example.urbanizat.utils
 
+
 import androidx.compose.ui.unit.dp
 
 
 //Styles of components
 val ROUNDED_CORNERS = 12.dp
+val ROUNDED_BUTTON_CORNERS = 8.dp
 val SPACER_HEIGHT = 12.dp
 val SPACE_INPUTS_FORM = 8.dp
