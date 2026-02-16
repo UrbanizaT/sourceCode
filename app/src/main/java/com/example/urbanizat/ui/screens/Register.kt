494e5d033eebe926e6e0ff41de313159e7376b89
Aleksander <aprokhorenkodev@gmail.com>
2026-02-16 11:31:44 +0100

    COMPONENTS | refactor: optimice the code using the new components. #6

--- app/src/main/java/com/example/urbanizat/ui/screens/Register.kt
+++ app/src/main/java/com/example/urbanizat/ui/screens/Register.kt
@@ -1,94 +1,45 @@
 package com.example.urbanizat.ui.screens
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.withLink
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.urbanizat.R
 import com.example.urbanizat.ui.components.buttons.BaseButton
 import com.example.urbanizat.ui.components.forms.InputCheckbox
 import com.example.urbanizat.ui.components.forms.InputCustom
+import com.example.urbanizat.ui.components.layouts.FormBody
+import com.example.urbanizat.ui.components.layouts.FormHeader
+import com.example.urbanizat.ui.components.layouts.MainColumn
 import com.example.urbanizat.ui.theme.UrbanizaTTheme
 import com.example.urbanizat.utils.ButtonColorsBase
 import com.example.urbanizat.utils.LinkStyles
-import com.example.urbanizat.utils.ROUNDED_CORNERS
 import com.example.urbanizat.utils.SPACER_HEIGHT
-import com.example.urbanizat.utils.SPACE_INPUTS_FORM
 
 @Composable
 fun RegisterScreen() {
-    Column(
-        modifier = Modifier
-            .background(color = MaterialTheme.colorScheme.background)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    MainColumn() {
         Column(
             modifier = Modifier
                 .fillMaxWidth(0.8f)
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Box(
-                    modifier = Modifier.weight(0.5f)
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.urbanizate),
-                        contentDescription = "Logo of UrbanizaT",
-                        modifier = Modifier
-                            .size(175.dp)
-                            .aspectRatio(16f / 9f)
-                            .fillMaxSize()
-                    )
-                }
-                Text(
-                    text = "Crear una cuenta",
-                    modifier = Modifier.weight(0.5f),
-                    fontSize = 18.sp
-                )
-            }
+            FormHeader(
+                label = "Crear una cuenta"
+            )
         }
         Spacer(modifier = Modifier.height(SPACER_HEIGHT))
-        Column(
-            modifier = Modifier
-                .background(
-                    color = MaterialTheme.colorScheme.secondary,
-                    shape = RoundedCornerShape(ROUNDED_CORNERS)
-                )
-                .fillMaxWidth(0.9f)
-                .wrapContentHeight()
-                .padding(vertical = 16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(SPACE_INPUTS_FORM)
-        )
-        {
+
+        FormBody() {
             InputCustom(
                 value = "Manolo", //TODO: add viewModel logic
                 onValueChange = {}, //TODO: add viewModel logic
@@ -140,7 +91,7 @@ fun RegisterScreen() {
                             LinkAnnotation.Clickable(
                                 tag = "terminos y condiciones",
                                 styles = LinkStyles.primary()
-                            ){
+                            ) {
                                 //TODO: añadir link de navegación
                             }
                         ) {
@@ -157,7 +108,7 @@ fun RegisterScreen() {
                         LinkAnnotation.Clickable(
                             tag = "loggin",
                             styles = LinkStyles.primary()
-                        ){
+                        ) {
                             //TODO: añadir link de navegación
                         }
                     ) {
